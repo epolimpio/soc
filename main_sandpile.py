99a0121a9bff9cbde1c6269665269304e612d4fa
Agustin <f.agustin@gmail.com>
2015-05-12 12:40:21 +0200

    plot of clusters and z

--- main_sandpile.py
+++ main_sandpile.py
@@ -4,21 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sandpile
 
-nx = 5
-ny = 5
-grain_number = 1000
+nx = 20
+ny = 20
+grain_number = 10000
 
 pile = sandpile.sandpile2D(nx, ny)
 plt.ion()
+
+fig0 = plt.figure()
+fig1 = plt.figure()
+ax0 = fig0.add_subplot(1,1,1)
+ax1 = fig1.add_subplot(1,1,1)
+
+cplt1 = ax1.imshow(pile.z, interpolation = 'none', cmap = 'Oranges', vmax = 4, vmin = 0)
+bar = plt.colorbar(cplt1, orientation = 'vertical')
 
 for grain in range(grain_number):
 	
 	if (pile.throw_sand() > pile.z_critical):
 		pile.avalanche()
 
-	plt.clf()
-	cplt = plt.contourf(np.arange(nx), np.arange(ny), pile.z)
-	bar = plt.colorbar(cplt, orientation = 'vertical')
+
+	ax0.clear()
+	cplt0 = ax0.imshow(pile.last_cluster.astype(int),  cmap='Greys', interpolation = 'none')
+	#print pile.last_cluster.astype(int)
+	plt.show()
+
+	ax1.clear()
+	cplt1 = ax1.imshow(pile.z, interpolation = 'none', cmap = 'Oranges', vmax = 4, vmin = 0)
 	plt.draw()
 
 
