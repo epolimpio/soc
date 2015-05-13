6a94f6b4af578a85a869702f4297e240b57d7bf2
epolimpio <epolimpio@gmail.com>
2015-05-13 22:38:54 +0200

    Avalanche

--- sandpile_heights.py
+++ sandpile_heights.py
@@ -3,17 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
 import avalanche
 
 nx = 10
 ny = 10
 grain_number = 1000
-critical_slope = 4
 
 pile = avalanche.avalanche2D(nx, ny)
 
 for grain in range(grain_number):
 	
-	if (pile.throw_sand() > critical_slope):
+	if (pile.throw_sand() > pile.sigma_critical):
 		pile.avalanche()
 
