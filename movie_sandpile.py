7e6d494ecc4ddadac04d692a7fc0cc4953006bc7
epolimpio <epolimpio@gmail.com>
2015-05-14 16:59:19 +0200

    Add presentation

--- movie_sandpile.py
+++ movie_sandpile.py
@@ -9,11 +9,19 @@ nx = 50
 ny = 50
 grain_number = 50000
 
-pile = sandpile.sandpile2D(nx, ny, movie = True)
+pile = sandpile.sandpile2D(nx, ny, movie = False)
+
+fig0 = plt.figure()
+ax0 = fig0.add_subplot(1,1,1)
 
 for grain in range(grain_number):
 	
 	if (pile.throw_sand() > pile.sigma_critical):
 		pile.avalanche()
+		if np.sum(pile.last_cluster) > 100:
+			break
 
 
+ax0.clear()
+cplt0 = plt.imshow(pile.last_cluster.astype(int),  cmap='Greys', interpolation = 'none')
+plt.show()
